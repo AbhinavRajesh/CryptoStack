765d029998de25997bd11cf64047362ab4c4f38d
UltimateRoman <59477486+UltimateRoman@users.noreply.github.com>
2022-02-11 23:47:24 +0530

    Initialized truffle project

---
+++ truffle/truffle-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    }
+  },
+
+  compilers: {
+    solc: {
+      version: "0.8.1",
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
